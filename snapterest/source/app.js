fa288f25e82eb6f352f25a73a5780d369f8f75d1
AlexDrozd <shuradrozd@mail.ru>
2017-10-03 18:20:42 +0300

    first commit

---
+++ snapterest/source/app.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var listOfItems =
+<ul className='list-of-items'>
+<li className='item-1'>Item 1</li>
+</ul>;
+
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
