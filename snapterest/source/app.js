9e17e9e7bb7077a9b13f383ecbf8b913e494f2ea
AlexDrozd <shuradrozd@mail.ru>
2017-10-05 18:40:17 +0300

    add news commit

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -1,8 +1,75 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var listOfItems =
-<ul className='list-of-items'>
-<li className='item-1'>Item 1</li>
-</ul>;
+ const React = require('react');
+ const ReactDOM = require('react-dom');
+    let my_news = [];
+    // let my_news = [
+    //   {
+    //   author: 'Саша Печкин',
+    //   text: 'В четверг, четвертого числа...'
+    //   },
+    //   {
+    //   author: 'Просто Вася',
+    //   text: 'Считаю, что $ должен стоить 35 рублей!'
+    //   },
+    //   {
+    //   author: 'Гость',
+    //   text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    //   }
+    // ];
 
-ReactDOM.render(listOfItems, document.getElementById('react-application'));
+ class News extends React.Component {
+
+  render() {
+
+     const data =  this.props.data;
+      let newsTemplate;
+     if (data.length !== 0) {
+         newsTemplate = data.map((item, index) => {
+             return (
+                 <div key={index}>
+                     <p className='news_author'>{item.author}:</p>
+                     <p className='news_text'>{item.text}</p>
+                 </div>
+             )
+         });
+     } else {
+         newsTemplate = <p>No any news</p>
+     }
+
+
+
+    return (
+      <div className='News'>
+      {newsTemplate}
+      <strong>All news: {data.length}</strong>
+      </div>
+    )
+ }
+}
+
+  class Comments extends React.Component {
+
+  render() {
+    return(
+      <div className='comments'>
+      No News No Comments
+      </div>
+      );
+  }
+ }
+
+
+ class App extends React.Component {
+
+  render() {
+    return(
+      <div className='app'>
+      Hello World
+      <News data={my_news} />
+      <Comments />
+      </div>
+      );
+  }
+ }
+
+ ReactDOM.render(<App />, document.getElementById('react-application'));
+
